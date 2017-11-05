e149dcb7d91fb8569681c373830a70db121462ca
ling <lingfangao@hotmail.com>
2017-11-05 12:27:56 +0100

    cleaned up project build files and added them to gitignore

--- mqttbridge/src/main/java/mqtt/MqttConsumer.java
+++ mqttbridge/src/main/java/mqtt/MqttConsumer.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class MqttConsumer implements MqttCallback{
-    final static Logger logger = Logger.getLogger(MqttConsumer.class);
+    final static Logger logger = Logger.getLogger(MqttConsuvim .mer.class);
     ConcurrentLinkedQueue<Map.Entry<String, String>> queue;
 
     public MqttConsumer(ConcurrentLinkedQueue<Map.Entry<String, String>> queue){

52a4f472da9922b5e9a8e034e9b67c576adf7bbe
ling <lingfangao@gmail.com>
2017-12-19 17:07:07 +0100

    made a bts sensor provider

--- SINC/sensor/init.sh
+++
@@ -1,3 +0,0 @@
-gcloud container clusters create test --num-nodes=3
-kubectl run broker --image=eclipse-mosquitto --port=1883
-kubectl expose deployment broker --port=1883 --target-port=1883 --type=LoadBalancer

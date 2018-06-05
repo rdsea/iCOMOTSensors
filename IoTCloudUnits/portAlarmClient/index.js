f2db8b9c1eea72537e41ba2b1327e8a48d5f50b2
ling <lingfangao@gmail.com>
2018-06-05 11:08:08 +0200

    initial implementation of alarm client

---
+++ IoTCloudUnits/portAlarmClient/index.js
@@ -0,0 +1,34 @@
+const mqtt = require('mqtt');
+const logger = require('./src/logger')
+const config = require('./config');
+
+const handleAlarm = require('./src/pcsService');
+
+let client = mqtt.connect(config.alarmBroker);
+    
+client.on('connect', () => {
+    for(let i=0;i<config.alarmBroker.topics.length;i++){
+        client.subscribe("#");
+        logger.info(`now listening to alarms`);
+    }
+});
+
+client.on('message', (topic, message) => {
+    logger.info(`${message.toString()} received from topic: ${topic} client: ${config.clientId}`);
+		let data = message.toString();
+		try{
+			data = JSON.parse(data);
+		}catch(err){
+			logger.warn(`${message.toString()} received from ${config.clientId} is not valid JSON!`);
+        }
+        handleAlarm(data);
+});
+
+client.on('error', function(err) {
+    logger.error(`error occurred in client ${client.clientId}`);
+    logger.error(err.message);
+});
+
+client.on('close', () => {
+    logger.warn(`disconnected client no longer ingesting from ${config.topics}`);
+})
---
+++ IoTCloudUnits/portAlarmClient/src/pcsService.js
@@ -0,0 +1,114 @@
+const axios = require('axios');
+const mqtt = require('mqtt');
+const logger = require('./logger');
+const config = require("../config");
+
+let client = mqtt.connect(config.vesselBroker);
+
+actions = {
+    NOTIFY_PRESENCE_TERMINAL_AUTHORITY: "NOTIFY_PRESENCE_TERMINAL_AUTHORITY",
+    NOTIFY_PRESENCE_HARBOUR_AUTHORITY: "NOTIFY_PRESENCE_HARBOUR_AUTHORITY",
+    COMMENCE_EVACUATION: "COMMENCE_EVACUATION",
+    BROADCAST_ASSIST_REQUEST: "BROADCAST_ASSIST_REQUEST",
+    REQUEST_NEW_TERMINAL: "REQUEST_NEW_TERMINAL",
+}
+
+function alarmAction(alarm){
+    logger.info(`received alarm ${alarm.alarm_type}`);    
+    logger.info(`requesting all vessels in ${alarm.terminal}`);
+
+    return axios(`${config.pcs}/vessels/${alarm.terminal}`).then((vessels) => {
+        logger.info(`${vessels.length} vessels found in ${alarm.terminal}`);
+        vessels.forEach((vessel) => {
+            // match alarm types and severity to boat status
+            switch(alarm.alarm_type){
+                case "FIRE":
+                    _handleFire(alarm, vessel);
+                    break;
+                case "POWER":
+                    _handlePower(alarm, vessel);
+                    break;
+                case "CHEMICAL":
+                    _handleChemical(alarm, vessel);
+                    break;
+                case "TRAFFIC":
+                    _handleTraffic(alarm, vessel);
+                    break;
+            }
+        });
+    });
+}
+
+function _handleFire(alarm, vessel){
+    let topic = vessel.boat.replace(/ /g,'').toLowerCase()
+    switch(alarm.alarm_severity){
+        case "NOTICE":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            break;
+        case "WARNING":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            client.publish(topic, actions.BROADCAST_ASSIST_REQUEST);
+            break;
+        case "CRIICAL":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            client.publish(topic, actions.COMMENCE_EVACUATION);
+            break;
+    }
+}
+
+function _handlePower(alarm, vessel){
+    let topic = vessel.boat.replace(/ /g,'').toLowerCase()
+    switch(alarm.alarm_severity){
+        case "NOTICE":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            break;
+        case "WARNING":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            break;
+        case "CRIICAL":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            
+            break;
+    }
+}
+
+function _handleChemical(alarm, vessel){
+    let topic = vessel.boat.replace(/ /g,'').toLowerCase()
+    switch(alarm.alarm_severity){
+        case "NOTICE":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            client.publish(topic, actions.REQUEST_NEW_TERMINAL);
+            break;
+        case "WARNING":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            client.publish(topic, actions.BROADCAST_ASSIST_REQUEST);
+            break;
+        case "CRIICAL":
+            client.publish(topic, actions.NOTIFY_PRESENCE_TERMINAL_AUTHORITY);
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            client.publish(topic, actions.COMMENCE_EVACUATION);
+            break;
+    }
+}
+
+function _handleTraffic(alarm, vessel){
+    let topic = vessel.boat.replace(/ /g,'').toLowerCase()
+    switch(alarm.alarm_severity){
+        case "NOTICE":
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            break;
+        case "WARNING":
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            break;
+        case "CRIICAL":
+            client.publish(topic, actions.NOTIFY_PRESENCE_HARBOUR_AUTHORITY);
+            client.publish(topic, actions.REQUEST_NEW_TERMINAL);
+            break;
+    }
+}
+module.exports = alarmAction;
+

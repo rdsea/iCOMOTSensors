b8b5e1fa86be63a35743b4c64472800bed2b1bb9
ling <lingfangao@gmail.com>
2018-06-11 18:14:39 +0200

    enable networking in cluster

--- utils/clustersetup/create-cluster.sh
+++ utils/clustersetup/create-cluster.sh
@@ -7,7 +7,7 @@ gcloud -q config set project iotcloudexamples
 gcloud -q config set compute/zone europe-west1-b
 
 # create our cluster of 5 instances
-gcloud -q container clusters create iotcloudexamples --num-nodes 5
+gcloud -q container clusters create iotcloudexamples --num-nodes 5 --enable-network-policy
 
 # get cluster credentials to interact
 gcloud -q container clusters get-credentials iotcloudexamples

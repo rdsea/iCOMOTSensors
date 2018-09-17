f4899db422d637b87a86e6d41b167ac582858b9d
Hong-Linh Truong <linhsolar@gmail.com>
2018-09-17 23:55:54 +0200

    add a simple utility

---
+++ utils/plainmqttsubscriber.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var topic = process.argv[2];
+var url = process.argv[3];
+var mqtt = require('mqtt');
+//load mqtt configuration
+var option = {
+    "clean": true
+};
+
+
+var mqttclient = mqtt.connect(url, option);
+//connect to the gateway - what if it fails
+mqttclient.on('connect', function () {
+    console.log("connected");
+    mqttclient.subscribe(topic);
+    console.log("Wait for the message");
+    //client.publish('presence', 'Hello mqtt');
+});
+//we must generalize the case, so thaht the client just write the transformation and the publish
+mqttclient.on('message', function (topic, message) {
+    console.log(topic, message.toString("utf-8"));
+});

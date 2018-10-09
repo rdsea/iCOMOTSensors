ffda4d362be9fcaa4e8413b13128feaedbd28cca
Michael Hammerer <m.hammerer27@gmail.com>
2018-10-09 08:35:07 +0200

    added bridge examples

--- IoTCloudUnits/node_red_dataflows/pizza_create_artefacts.sh
+++ IoTCloudUnits/node_red_dataflows/pizza_create_artefacts.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+#bridgeelements
+#artefacts for xml_to_yaml bridge
+pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/bridges/xml_to_yaml_bridge/bridgeelement_json_to_yaml.json nodered ./bridges/xml_to_yaml_bridge/metadata_bridgeelement_json_to_yaml.json bridgeelement_json_to_yaml
+pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/bridges/xml_to_yaml_bridge/bridgeelement_xml_to_json.json nodered ./bridges/xml_to_yaml_bridge/metadata_bridgeelement_xml_to_json.json bridgeelement_xml_to_json
+
+#artefacts for yaml_to_csv bridge
+pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/bridges/yaml_to_csv_bridge/bridgeelement_json_to_csv.json nodered ./bridges/yaml_to_csv_bridge/metadata_bridgeelement_json_to_csv.json bridgeelement_json_to_csv
+pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/bridges/yaml_to_csv_bridge/bridgeelement_yaml_to_json.json nodered ./bridges/yaml_to_csv_bridge/metadata_bridgeelement_yaml_to_json.json bridgeelement_yaml_to_json
+
+
+#artefact flows
 pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/csv_to_json_flow/flow_csv_to_json.json nodered ./csv_to_json_flow/metadata_csv_to_json.json nodered_csv_to_json
 pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/csv_to_xml_flow/flow_csv_to_xml.json nodered ./csv_to_xml_flow/metadata_csv_to_xml.json nodered_csv_to_xml
 pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/fixed_header_csv_to_json_flow/flow_fixed_header_csv_to_json_flow.json nodered ./fixed_header_csv_to_json_flow/metadata_fixed_header_csv_to_json.json nodered_fixed_header_csv_to_json_flow
@@ -10,3 +21,6 @@ pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/ma
 pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/xml_to_csv_flow/flow_xml_to_csv_flow.json nodered ./xml_to_csv_flow/metadata_xml_to_csv.json nodered_xml_to_csv_flow
 pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/xml_to_json_flow/flow_xml_to_json_flow.json nodered ./xml_to_json_flow/metadata_xml_to_json.json nodered_xml_to_json_flow
 pizza artefact create https://raw.githubusercontent.com/rdsea/IoTCloudSamples/master/IoTCloudUnits/node_red_dataflows/yaml_to_json_flow/flow_yaml_to_json_flow.json nodered ./yaml_to_json_flow/metadata_yaml_to_json.json nodered_yaml_to_json_flow
+
+
+

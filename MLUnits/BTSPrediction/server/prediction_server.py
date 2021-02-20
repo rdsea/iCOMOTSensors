ac63bb89718753d4d270d8d415f0e93b464100cb
Linh Nguyen <linh.m.nguyen@aalto.fi>
2021-02-20 16:33:02 +0200

    Graduate BTSprediction unit

--- MLUnits/BTSPrediction/server/prediction_server.py
+++ MLUnits/BTSPrediction/server/prediction_server.py
@@ -4,24 +4,12 @@ import argparse
 import time
 import json
 import pika
-import sys
-sys.path.append("../probeslib")
-from probes import Qoa_Client
-
-client_info = {"id": "aaltosea1",
-        "roles": "ml_provider"}
-service_info = {"id": "request", 
-        "machinetypes": "small", 
-        "metric": ["Accuracy", "ResponseTime", "DataAccuracy"]}
-metric = {"Accuracy": 100.0,
-        "ResponseTime": 0.0}
-ground_truth = [1160629000, 121, 308]
 
 
 flag = True
 
 class ML_Prediction_Server(object):
-    def __init__(self, qoa_url):
+    def __init__(self):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel = self.connection.channel()
 
@@ -30,10 +18,6 @@ class ML_Prediction_Server(object):
         self.result = self.channel.queue_declare(queue='rpc_queue', exclusive=True)
         self.queue_name = self.result.method.queue
         self.channel.queue_bind(exchange='topic_bts', queue=self.queue_name, routing_key="request.#")
-        self.qoa_client = Qoa_Client(client_info,service_info)
-        self.qoa_url = qoa_url
-
-        
 
     def on_request(self, ch, method, props, body):
         start_time = time.time()
@@ -65,10 +49,6 @@ class ML_Prediction_Server(object):
                         body=str(response))
         response_time = time.time()-start_time
         input_data = [float(predict_value["station_id"]), float(predict_value["data_point"]), float(predict_value["alarm_id"])]
-        data_accuracy = self.qoa_client.data_quality(ground_truth,input_data)
-        metric["ResponseTime"] = response_time
-        metric["DataAccuracy"] = data_accuracy
-        qoa_response = self.qoa_client.qoa_report(metric, self.qoa_url)
         ch.basic_ack(delivery_tag=method.delivery_tag)
     def run(self):
         self.channel.basic_qos(prefetch_count=1)
@@ -76,7 +56,7 @@ class ML_Prediction_Server(object):
         self.channel.start_consuming()
 
     def single_var_LR(self, index):
-        interpreter = tflite.Interpreter("../exported_models/tflite_model/single_var_LR.tflite")
+        interpreter = tflite.Interpreter("../models/single_var_LR/single_var_LR.tflite")
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
@@ -87,7 +67,7 @@ class ML_Prediction_Server(object):
         return y
 
     def multi_var_LR(self, indext, value, thresdhold):
-        interpreter = tflite.Interpreter("../exported_models/tflite_model/multi_var_LR.tflite")
+        interpreter = tflite.Interpreter("../models/multi_var_LR/multi_var_LR.tflite")
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
@@ -98,7 +78,7 @@ class ML_Prediction_Server(object):
         return y
 
     def DNN_single_regression(self, index):
-        interpreter = tflite.Interpreter("../exported_models/tflite_model/DNN_single_regression.tflite")
+        interpreter = tflite.Interpreter("../models/DNN_single_regression/DNN_single_regression.tflite")
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
@@ -109,7 +89,7 @@ class ML_Prediction_Server(object):
         return y
 
     def DNN_multi_regression(self, indext, value, thresdhold):
-        interpreter = tflite.Interpreter("../exported_models/tflite_model/DNN_multi_regression.tflite")
+        interpreter = tflite.Interpreter("../models/DNN_multi_regression/DNN_multi_regression.tflite")
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
@@ -119,21 +99,11 @@ class ML_Prediction_Server(object):
         y = interpreter.get_tensor(output_details[0]['index']) 
         return y
 
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Predict on edge device")
-    parser.add_argument('--url', help='qoa4ml OPA service', default='http://0.0.0.0:8181/v1/data/qoa4ml/bts/alarm/violation')
-    args = parser.parse_args()
-
-    
-    ml_prediction_server = ML_Prediction_Server(args.url)
+    ml_prediction_server = ML_Prediction_Server()
     ml_prediction_server.run()
-    
-    
 
-    # client.subscribe("alarm/finish")
     print("Start Scribing")
     while (flag):
         print("waiting")
